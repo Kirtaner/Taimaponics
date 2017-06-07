1ef1213ac7f44ab8167a5ae0c97e9ac6caf3e386
Aubrey Cottle <acottle@taimalabs.com>
2017-06-06 21:25:13 -0400

    Add base angular templating Header footer, navbar stubs + routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
+
+// @Component({
+//   selector: 'app-root',
+//   styleUrls: ['./app.component.css']
+// })
 export class AppComponent {
   title = 'app works!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { SerialComponent } from './serial/serial.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+  { path: '', component: DashboardComponent }
+  // { path: 'login', component:  },
+  // { path: 'logout', component:  },
+  // { path: '',   redirectTo: '/', pathMatch: 'full' },
+  // { path: '**', component:  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    SerialComponent
+    SerialComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
