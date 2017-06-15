ee285a32f19b86d18f2e3ed2229cfc965328de95
Aubrey Cottle <acottle@taimalabs.com>
2017-06-15 16:08:43 -0400

    Rendering sensor data!

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,15 +8,24 @@ import { SerialService } from '../common/serial.service';
   providers: [ SerialService ]
 })
 export class DashboardComponent implements OnInit, OnDestroy {
-  sensors;
   connection;
+  sensors;
+
+  airTemperature;
+  humidity;
+  waterTemperature;
+  relayStatus;
 
   constructor(private serialService: SerialService) { }
 
   ngOnInit() {
     this.connection = this.serialService.getSensors().subscribe(message => {
       this.sensors = message;
-    })
+      this.airTemperature = this.sensors[0];
+      this.humidity = this.sensors[1];
+      this.waterTemperature = this.sensors[2];
+      this.relayStatus = this.sensors[3];
+    });
   }
 
   ngOnDestroy() {
