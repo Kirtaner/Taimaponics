f51badd94be9a0cb0416e280007558de4efc4730
Aubrey Cottle <acottle@taimalabs.com>
2017-06-22 16:59:42 -0400

    refactor socket.js

--- app/socket.js
+++ app/socket.js
@@ -3,18 +3,20 @@ var io = global.io;
 var serial = global.serial;
 var sockets = {};
 
+function emitSensorData() {
+  io.emit('sensors', serial.sensors);
+}
+
 io.on('connection', function(socket) {
 
   sockets[socket.id] = socket;
   console.log("Total clients connected:", Object.keys(sockets).length);
 
+  emitSensorData();
+
   socket.on('disconnect', function() {
     delete sockets[socket.id];
     console.log("Client disconnected");
-  });
-
-  socket.on('hello', function(data) {
-    console.log(data);
   });
 
   socket.on('relayOn', function(relay) {
@@ -26,22 +28,4 @@ io.on('connection', function(socket) {
   });
 });
 
-function emitSensorData() {
-  io.emit('sensors', serial.sensors);
-}
-
 setInterval(emitSensorData, 2000);
-
-// app.get('/arduino/relay/on/:port', function(req, res) {
-//   var port = req.params.port;
-//   var command = 'o'+port;
-//   serialPort.write(command);
-//   res.send('relay on');
-// });
-
-// app.get('/arduino/relay/off/:port', function(req, res) {
-//   var port = req.params.port;
-//   var command = 'c'+port;
-//   serialPort.write(command);
-//   res.send('relay off');
-// });
