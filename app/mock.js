a52d0d9392ab7490c6d892f2cab83a75573fac0e
Aubrey Cottle <acottle@taimalabs.com>
2017-06-25 21:57:16 -0400

    eliminate string parsing from mock

--- app/mock.js
+++ app/mock.js
@@ -1,12 +1,20 @@
 const global = require('./global');
 var config = global.config;
 
-var relayStatus = "00000000";
 var sensors = {
   roomTemperature: 0,
   relativeHumidity: 0,
   waterTemperature: 0,
-  relays: Array.from(relayStatus.toString()).map(Number)
+  relays: [
+    true,
+    false,
+    true,
+    false,
+    true,
+    false,
+    true,
+    false
+  ]
 };
 
 console.log('Mock serial connection active');
@@ -19,24 +27,17 @@ function randomInt(low, high)
 // modifies relay activation state list
 function setRelayStatus(mode, relay)
 {
-  console.log('setRelayStatus - Relay: ' + relay);
-  console.log('setRelayStatus - Mode: ' + mode);
-
-  relayStatus = relayStatus.substr(0,relay) + mode + relayStatus.substr(relay+1);
+  sensors.relays[relay] = mode;
 }
 
 function updateSensors()
 {
-  let string = mockSerial.getRoomTemperature() + " " + mockSerial.getHumidity() + " " + mockSerial.getWaterTemperature() + " " + mockSerial.getRelayStatus();
+  let string = mockSerial.getRoomTemperature() + " " + mockSerial.getHumidity() + " " + mockSerial.getWaterTemperature();
   let sensorArray = string.split(" ");
-  let relayArray = Array.from(sensorArray[3].toString()).map(Number);
 
   sensors.roomTemperature = sensorArray[0];
   sensors.relativeHumidity = sensorArray[1];
   sensors.waterTemperature = sensorArray[2];
-  sensors.relays = relayArray;
-
-  module.exports.sensors = sensors;
 }
 
 /**
@@ -65,31 +66,32 @@ mockSerial =  {
     let commandIntent = command.charAt(0);
 
     if(commandIntent == 'o') {
-      setRelayStatus(1, command.charAt(1));
+      setRelayStatus(true, command.charAt(1));
     }
     if (commandIntent == 'c') {
-      setRelayStatus(0, command.charAt(1));
+      setRelayStatus(false, command.charAt(1));
     }
-    console.log("relayStatus: " + relayStatus);
   },
 
   activateRelay(relay) {
     this.serialCommand('o'+relay);
-    return relayStatus;
+    return sensors.relays;
   },
 
   deactivateRelay(relay) {
     this.serialCommand('c'+relay);
-    return relayStatus;
+    return sensors.relays;
   },
 
   getRelayStatus() {
-    return relayStatus;
+    return sensors.relays;
   },
 }
 
+setInterval(updateSensors, 2000);
+
 module.exports = mockSerial;
+module.exports.sensors = sensors;
 
 // Update the mock sensors!
-updateSensors();
-setInterval(updateSensors, 2000);
+// updateSensors();
