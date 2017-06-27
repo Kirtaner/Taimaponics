6368d25baa2684796be70a481bbcc700148acd96
Aubrey Cottle <acottle@taimalabs.com>
2017-06-27 15:39:39 -0400

    application settings model

---
+++ app/models/settings.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+
+const settingsSchema = Schema({
+  system: {
+    serial: {
+      _id: false,
+      port: String,
+      baudRate: Number,
+      dataBits: { type: Number, min: 5, max: 8 },
+      stopBits: { type: Number, min: 1, max: 2 },
+      parity: String
+    },
+  },
+  logging: {
+    logInterval: Number
+  }
+});
+
+module.exports = mongoose.model('Settings', settingsSchema);
